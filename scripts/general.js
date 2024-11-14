bebf4c5e9db1976cd48005231cb89fdab4ea09e1
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-11-14 10:24:05 -0300

    Renamed General and removed funnies from all files

--- scripts/general.js
+++ scripts/general.js
@@ -1,26 +1,3 @@
-let data = {
-  "trail":true
-}
-
-// funnies
-function trail (e) {
-	var div = document.createElement('div');
-  
-  div.classList.add('happyTrail');
-  
-  Object.assign(div.style, {
-    top: e.pageY + 'px',
-    left: e.pageX + 'px'
-  });
-  
-  document.body.appendChild(div);
-  setTimeout(function(){document.body.removeChild(div)}, 290);
-  if (e.pageX >= 1.2*body.offsetWidth) {
-    alert("Another One")
-  }
-}
-addEventListener('mousemove', trail);
-
 
 function includeHTML() {
     var z, i, elmnt, file, xhttp;
@@ -51,16 +28,3 @@ function includeHTML() {
   }
 
 includeHTML()
-
-const body = document.getElementById("body")
-body.onkeydown = (e) => {
-  if (e["key"] == "t") {
-    if (data["trail"]){
-      removeEventListener('mousemove', trail);
-      data["trail"] = false
-    } else {
-      addEventListener('mousemove', trail);
-      data["trail"] = true
-    }
-  }
-}
