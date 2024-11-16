a7c83d79d393533a970b11a647aa8248e1e57e38
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-11-16 13:00:32 -0300

    Added a 'george' mode that adds lots of hearts to the website

--- scripts/funnies.js
+++ scripts/funnies.js
@@ -3,11 +3,15 @@ export const Funnies = () => {
   let data = {
     "debug":false,
     "debug_list":[],
-    "trail":false,
+    "george":false,
+    "george_list":[],
+    "happyTrail":false,
     "secret_letters":false
   }
 
-  function trail (e) {
+  const body = document.getElementById("body")
+
+  function happyTrail (e) {
       var div = document.createElement('div');
 
       div.classList.add('happyTrail');
@@ -19,19 +23,79 @@ export const Funnies = () => {
 
       document.body.appendChild(div);
       setTimeout(function(){document.body.removeChild(div)}, 290);
-      
-      // SECRET OFF SCREEN BY WIDTH
-      if (e.pageX >= 1.2*body.offsetWidth) {
-          alert("Another One")
-      }
   }
 
-  const enableTrail = () => {
-      data["trail"] = true
-      addEventListener('mousemove', trail);
+  // GEORGE 
+  const addHearts = () => {
+    let titlesList = document.getElementsByTagName("h1")
+
+    for (let i = 0; i < titlesList.length; i++) {
+      const title = titlesList[i];
+      title.innerHTML = "❤" + title.innerHTML + "❤"
+    }
+
+  }
+
+  const enableHappyTrail = () => {
+      data["happyTrail"] = true
+      addEventListener('mousemove', happyTrail);
 
       alert("Parabens! Ganhou uma calda pro mouse.. Você pode ativar/desativar apertando a tecla T.")
   }
+
+  let randHeart = () => {
+    let r_width = Math.random() * document.documentElement.clientWidth;
+    let r_height = Math.random() * document.documentElement.clientHeight;
+
+    var div = document.createElement('div');
+
+    div.classList.add('heart');
+    div.classList.add('big');
+
+    Object.assign(div.style, {
+        top: r_width + 'px',
+        left: r_height + 'px'
+    });
+
+    document.body.appendChild(div);
+
+    setTimeout(function(){ document.body.removeChild(div) }, 4000);
+    setTimeout(function(){randHeart()}, 500);
+  }
+
+  let running = false;
+  function heartTrail (e) {
+    if (running == true) {return}
+    running = true;
+    var div = document.createElement('div');
+
+    div.classList.add('heart');
+
+    Object.assign(div.style, {
+        top: e.pageY + 'px',
+        left: e.pageX + 'px'
+    });
+
+    document.body.appendChild(div);
+    setTimeout(function(){running = false}, 30);
+    setTimeout(function(){document.body.removeChild(div)}, 500);
+  }
+
+const enableGeorgeMode = () => {
+    data["george"] = true;
+    addEventListener('mousemove', heartTrail);
+    randHeart()
+    addHearts()
+
+    // alert("Parabens! Ganhou uma calda pro mouse.. Você pode ativar/desativar apertando a tecla T.");
+}
+
+  const secretOffScreen = (e) => {
+    if (e.pageX >= 1.2*body.offsetWidth) {
+      alert("Another One")
+    }
+  }
+  addEventListener('mousemove', secretOffScreen);
 
   // LETTERS SECRET
   let letters = []
@@ -44,7 +108,7 @@ export const Funnies = () => {
     // SECRET LETTERS FOUND
     if (letters.length >= 7) {
       data["secret_letters"] = true
-      enableTrail()
+      enableHappyTrail()
     }
   }
 
@@ -55,29 +119,36 @@ export const Funnies = () => {
   }
 
   // LETTERS KEYPRESS
-  const body = document.getElementById("body")
   body.onkeydown = (e) => {
     if (e["key"] == "t" && data["secret_letters"]) {
-      if (data["trail"]){
+      if (data["happyTrail"]){
         removeEventListener('mousemove', trail);
-        data["trail"] = false
+        data["happyTrail"] = false
       } else {
         addEventListener('mousemove', trail);
-        data["trail"] = true
+        data["happyTrail"] = true
       }
     }
 
-      if (["d", "e", "b", "u", "g"].includes(e["key"])) {
-          data["debug_list"].push(e["key"])
-          
-          if (data["debug_list"].join("") == "debug") {
-              for (const item in data) {
-                  if (data[item] == false) {data[item] = true}
-              }
-              enableTrail()
-              console.log("DEBUG MODE ACTIVATED.")
-          }
+    if ("george".split("".includes(e["key"]))) {
+      data["george_list"].push(e["key"])
+      if (data["george_list"].join("") == "george") {
+        data["george"] = true
+        enableGeorgeMode()
       }
+    }
+
+    if ("debug".split("").includes(e["key"])) {
+        data["debug_list"].push(e["key"])
+        
+        if (data["debug_list"].join("") == "debug") {
+            for (const item in data) {
+                if (data[item] == false) {data[item] = true}
+            }
+            enableHappyTrail()
+            console.log("DEBUG MODE ACTIVATED.")
+        }
+    }
 
   }
 
