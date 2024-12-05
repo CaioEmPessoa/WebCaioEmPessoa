b48e194066d5b9c6e9563e254d82288c35a42429
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-12-05 10:10:46 -0300

    Removed index scripts from main import script

--- scripts/main_script.js
+++ scripts/main_script.js
@@ -1,8 +1,6 @@
 // transform this ones into classes
 import { Funnies } from "./funnies.js";
 import { General } from "./general.js";
-import { Index } from "./index.js";
 
 General()
 Funnies()
-Index()
