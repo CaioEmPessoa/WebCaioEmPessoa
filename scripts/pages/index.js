54f7ba53b90ed09f8fa10a7faad7ce4cbb27bb4f
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-12-05 10:06:30 -0300

    Only index imports index.

---
+++ scripts/pages/index.js
@@ -0,0 +1,44 @@
+function shuffle(array) {
+    let currentIndex = array.length;
+    
+    // While there remain elements to shuffle...
+    while (currentIndex != 0) {
+    
+        // Pick a remaining element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+    
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+}
+
+const changePfp = (dir) => {
+    console.log(imgIndex)
+    
+    if (dir) {
+        if (imgIndex >= imgs.length-1) { imgIndex = 0; }
+        else {imgIndex += 1; }
+    } else {
+        if (imgIndex <= 0) { imgIndex = imgs.length - 1; }
+        else {imgIndex -= 1; }
+    }
+    pfpImg.src = "/src/imgs/pfp/" + imgs[imgIndex];
+}
+
+let imgs = [];
+let imgIndex = 0;
+const pfpImg = document.getElementById("pfp-img");
+
+$.getJSON('/src/imgs/pfp/', img_l => {
+    shuffle(img_l);
+    imgs = img_l
+    pfpImg.src = "/src/imgs/pfp/" + imgs[imgIndex];
+
+    document.getElementById("right-arrow").onclick = () => {changePfp(1)};
+    document.getElementById("left-arrow").onclick = () => {changePfp(0)};
+});
+
+
+
