cb17da6fdfc01da5fc80f69a9097c528fdb430e5
CaioEmPessoa <caiopolonioabruno@gmail.com>
2024-12-12 18:34:17 -0300

    minor content and bug updates

--- src/imgs/pfp/list.py
+++ src/imgs/pfp/list.py
@@ -7,6 +7,8 @@ current_directory = os.path.dirname(os.path.abspath(__file__))
 # List all files in the current directory
 files = [f for f in os.listdir(current_directory) if os.path.isfile(os.path.join(current_directory, f))]
 
+files.remove("list.py")
+
 # Save the file list to info.json
 with open(os.path.join(current_directory, 'index.html'), 'w') as json_file:
     json.dump(files, json_file, indent=4)
